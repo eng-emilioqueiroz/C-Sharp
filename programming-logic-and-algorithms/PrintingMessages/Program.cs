e066c40664ec7b2d1b973fa5fd71288d2263f80d
Emílio <emilioqueiroz@gmail.com>
2025-11-23 17:33:04 -0300

    create first code in c# to printing messages

--- programming-logic-and-algorithms/PrintingMessages/Program.cs
+++ programming-logic-and-algorithms/PrintingMessages/Program.cs
@@ -1,26 +1,29 @@
 ﻿using System;
 using System.Globalization;
+
 namespace FirstProgram {
-    class FirstProgram {
+    class PrintingMessages {
         static void Main(string[] args) {
             //declaring variables
             double x = 10.35784;
             int y = 32;
             string z = "Maria";
-            char w = 'F';
+            char w = 'M';
             //printing messages
-            Console.Write("Olá mundo!"); //print without newline
-            Console.WriteLine("Bom dia!"); //print with newline
-            Console.WriteLine("Até mais!");
+            Console.Write("Hello World!"); //print without newline
+            Console.WriteLine("Good Morning!"); //print with newline
+            Console.WriteLine("See you later!");
             Console.WriteLine(x);
             Console.WriteLine(x.ToString("F2")); //formatting to 2 decimal places
             Console.WriteLine(x.ToString("F4")); // formatting to 4 decimal places
             Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture)); // formatting to 2 decimal places with invariant culture
-            Console.WriteLine("RESULTADO = " + x); // concatenation
-            Console.WriteLine("O valor do troco é " + x + " reais"); // concatenation
-            Console.WriteLine("O valor do troco é " + x.ToString("F2") + " reais"); // concatenation with formatting
-            Console.WriteLine($"A paciente {z} tem {y} anos e seu sexo é {w}"); // interpolation
+            Console.WriteLine("The x value is " + x); // concatenation
+            Console.WriteLine($"The x value is {x}"); // concatenation
+            Console.WriteLine("The x value is " + x.ToString("F2") + " reais"); // concatenation with formatting
+            Console.WriteLine($"The lady called {z} have {y} years and her nickname is {w}"); // interpolation
             Console.ReadLine();
         }
     }
 }
+
+
