532c63f65d069f91678c028ff8ec86e72e51ec76
Emílio <emilioqueiroz@gmail.com>
2026-01-03 22:57:52 -0300

    adding code to process data in c#

---
+++ programming-logic-and-algorithms/ProcessingData/ProcessingData.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ThirdProgram {
+    class ProcessingData {
+        static void Main(string[] args) {
+            int a, b;
+            double result;
+
+            a = 5;
+            b = 2;
+
+            result = (double)a / b; //casting to avoid integer division
+            Console.WriteLine($"Result of division: {result}");
+        }
+
+    }
+}
